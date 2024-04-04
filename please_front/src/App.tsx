d48bff09a0b2f8180be8cd824b25e78c11f5dea0
MrLobotomist <39457028+MrLobotomist@users.noreply.github.com>
2024-04-04 23:52:20 +0500

    04.04.2024 (debug)

--- please_front/src/App.tsx
+++ please_front/src/App.tsx
@@ -1,40 +1,37 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { lime, purple } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { setHomeLink } from "@store/common.ts";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
-import Index from "@/pages/index.tsx"
+import "./App.css";
+import Index from "@/pages/index.tsx";
+import { useDispatch } from "react-redux";
+import { lime, purple } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: lime,
     secondary: purple,
   },
-})
+});
 
 function App() {
   const dispatch = useDispatch();
-  const [count, setCount] = useState(0)
 
-  if (location.origin.includes("localhost")){
+  if (location.origin.includes("localhost")) {
     dispatch(setHomeLink("http://localhost:443"));
   } else {
     // Можно добавить другие origin
     // Если приложуха - часть домена можно забить prefix
-  } 
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element={<Index/>}/>
+          <Route path={"/"} element={<Index />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
