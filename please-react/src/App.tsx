038a092d77dc0433c1fe05a12e3c409aa20a9572
sae <alexxxkish@gmail.com>
2024-05-13 01:33:19 +0500

    Заменен src/pages/entitys файлом ../entitys Добавлен вывод списков в зависимости от выбранного пункта меню навигации

--- please-react/src/App.tsx
+++ please-react/src/App.tsx
@@ -6,7 +6,7 @@ import TopBar from './components/TopBar';
 import {useStores} from './context/root-store-general-context';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/home';
-import Entitys from './pages/entitys';
+import Entities from './pages/entities.tsx';
 import Entity from './pages/entity';
 import References from './pages/references';
 import Settings from './pages/settings';
@@ -32,8 +32,8 @@ const  App:React.FC = observer(() => {
 								<Box sx={{display:"flex", flexDirection:"column", transition: "margin 0.3s ease"}} marginTop={"70px"} marginLeft={SideBar.collapsed ? ("80px"):("250px")}>
 									<Routes>
 										<Route path='/' element={<Home></Home>}/>
-										<Route path='/candidate' element={<Entitys rentity_type_name='candidate'></Entitys>}/>
-										<Route path='/request' element={<Entitys rentity_type_name='request'></Entitys>}/>
+										<Route path='/candidate' element={<Entities rentity_type_name='candidate'></Entities>}/>
+										<Route path='/request' element={<Entities rentity_type_name='request'></Entities>}/>
 										<Route path='/candidate/:id' element={<Entity entity_id={1}></Entity>}/>
 										<Route path='/request/:id' element={<Entity entity_id={1}></Entity>}/>
 										<Route path='/references' element={<References></References>}/>
